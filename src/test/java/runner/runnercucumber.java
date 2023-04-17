dc578437353d1865374d6c1a270fc69b1fe00410
Kalpans77 <kalpanapadmakumar@yahoo.com>
2023-04-16 21:02:27 -0500

    Pushing modules

--- src/test/java/runner/runnercucumber.java
+++ src/test/java/runner/runnercucumber.java
@@ -3,33 +3,34 @@ package runner;
 import utils.Configreader;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+
+import io.cucumber.junit.CucumberOptions;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
+
+
+import org.junit.runner.RunWith;//
 import org.testng.annotations.Parameters;
-//import org.junit.runner.RunWith;
-import org.testng.annotations.Parameters;
 
 
-//import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.Cucumber;//
 
 
-//@RunWith(Cucumber.class)
-	
-       // @RunWith(Cucumber.class)
+@RunWith(Cucumber.class)//
+   
 		@CucumberOptions(features="src/test/resources/features"
 		,glue ={"stepDefination","hooks"}
-		,monochrome=true,//dryRun = false,  
+		,monochrome=true,dryRun = false,  
 		//tags = " @check",
 		publish=true,
 		plugin= {"pretty","html:target/cucumber.html"
 				,"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 		})
-		
+		public class runnercucumber{
+}
+
         //@Test
-		public class runnercucumber extends AbstractTestNGCucumberTests{
+	//	public class runnercucumber extends AbstractTestNGCucumberTests{
 
 		
 //			@Override
@@ -52,7 +53,7 @@ import org.testng.annotations.Parameters;
 ////				Configreader.setbrowser("Edge");
 ////				System.out.println("in Setup");		
 ////		}	*/
-	}
+//	}
 	
 
 
